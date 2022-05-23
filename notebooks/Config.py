dc42f32fe766a9c1d37af479192a6a49697c384b
Christina Aftzidis <caft@itu.dk>
2022-05-23 20:44:50 +0200

    Added data characterization and Config file

---
+++ notebooks/Config.py
@@ -0,0 +1,17 @@
+hate_train_text_path = '../dataset/hate/train_text.txt'
+hate_train_labels_path = '../dataset/hate/train_labels.txt'
+
+hate_val_text_path = '../dataset/hate/val_text.txt'
+hate_val_labels_path = '../dataset/hate/val_labels.txt'
+
+hate_test_text_path = '../dataset/hate/test_text.txt'
+hate_test_labels_path = '../dataset/hate/test_labels.txt'
+
+abortion_train_text_path = '../dataset/stance/abortion/train_text.txt'
+abortion_train_labels_path = '../dataset/stance/abortion/train_labels.txt'
+
+abortion_val_text_path = '../dataset/stance/abortion/val_text.txt'
+abortion_val_labels_path = '../dataset/stance/abortion/val_labels.txt'
+
+abortion_test_text_path = '../dataset/stance/abortion/test_text.txt'
+abortion_test_labels_path = '../dataset/stance/abortion/test_labels.txt'
